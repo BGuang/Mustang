fa90c75cd2209e26518b680d8cda8844e76f4df7
BGuang <583567546@qq.com>
2018-07-08 17:05:50 +0800

    Tool of redis. Base on StackExchange.Redis

---
+++ Mustang/Mustang.NoSql.Redis/RedisCache.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+
+namespace Mustang.NoSql.Redis
+{
+    public class RedisCache : ICache
+    {
+        private readonly IDatabase _db;
+        private readonly ConnectionMultiplexer _redis;
+        private int _dbnum;
+        public RedisCache():this(0,null)
+        {           
+        }
+        public RedisCache(int dbnum) : this(dbnum, null)
+        {
+        }
+        public RedisCache(int dbNum, string readWriteHosts)
+        {
+            _redis = string.IsNullOrWhiteSpace(readWriteHosts) ? RedisManager.Instance:RedisManager.GetConnectionMultiplexer(readWriteHosts);
+            _db = _redis.GetDatabase(_dbnum);
+        }
+
+        public object Get(string key)
+        {
+            return Get<object>(key);
+        }
+        public T Get<T>(string key)
+        {
+            var cacheValue = _db.StringGet(key);
+            var value = default(T);
+            if (cacheValue.HasValue && !cacheValue.IsNull)
+            {
+                var cacheObject = JsonConvert.DeserializeObject<T>(cacheValue);
+                value = cacheObject;
+            }
+            return value;
+        }
+
+        public void Insert(string key, object data)
+        {
+            var jsonData = SerializeContent(data);
+            _db.StringSet(key, jsonData);
+        }
+
+        public void Insert(string key, object data, int expiry)
+        {
+            var timeSpan = TimeSpan.FromSeconds(expiry);
+            var jsonData = SerializeContent(data);
+            _db.StringSet(key, jsonData, timeSpan);
+        }
+
+        public void Insert(string key, object data, TimeSpan expiresAt)
+        {
+            var jsonData = SerializeContent(data);
+            _db.StringSet(key, jsonData, expiresAt);
+        }
+
+        public void Insert<T>(string key, T data)
+        {
+            var jsonData = SerializeContent(data);
+            _db.StringSet(key, jsonData);
+        }
+
+        public void Insert<T>(string key, T data, int cacheTime)
+        {
+            var timeSpan = TimeSpan.FromSeconds(cacheTime);
+            var jsonData = SerializeContent(data);
+            _db.StringSet(key, jsonData, timeSpan);
+        }
+
+        public void Insert<T>(string key, T data, TimeSpan expiresAt)
+        {
+            var jsonData = SerializeContent(data);
+            _db.StringSet(key, jsonData, expiresAt);
+        }
+
+        public bool Update<T>(string key, T value) where T : class
+        {
+            var stringContent = SerializeContent(value);
+            return _db.StringSet(key, stringContent);
+        }
+
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(string key)
+        {
+            _db.KeyDelete(key, CommandFlags.HighPriority);
+        }
+
+        /// <summary>
+        /// 判断key是否存在
+        /// </summary>
+        public bool Exists(string key)
+        {
+            return _db.KeyExists(key);
+        }
+
+        #region private
+        private string SerializeContent(object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+
+        private T DeserializeContent<T>(RedisValue myString)
+        {
+            return JsonConvert.DeserializeObject<T>(myString);
+        }
+        #endregion
+
+    }
+}
---
+++ Mustang/Mustang.NoSql.Redis/RedisManager.cs
@@ -0,0 +1,151 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Configuration;
+
+namespace Mustang.NoSql.Redis
+{
+
+    public class RedisManager
+    {
+        private static readonly ConcurrentDictionary<string, ConnectionMultiplexer> ConnectionCache = new ConcurrentDictionary<string, ConnectionMultiplexer>();
+        #region 默认单例
+        /// <summary>
+        /// 锁对象
+        /// </summary>
+        private static object _locker = new object();
+        /// <summary>
+        /// StackExchange.Redis对象
+        /// </summary>
+        private static ConnectionMultiplexer instance;
+
+        /// <summary>
+        /// 得到StackExchange.Redis单例对象
+        /// </summary>
+        public static ConnectionMultiplexer Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (_locker)
+                    {
+                        if (instance != null)
+                            return instance;
+
+                        instance = GetManager();
+                        return instance;
+                    }
+                }
+
+                return instance;
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// 缓存获取
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static ConnectionMultiplexer GetConnectionMultiplexer(string connectionString)
+        {
+            if (!ConnectionCache.ContainsKey(connectionString))
+            {
+                ConnectionCache[connectionString] = GetManager(connectionString);
+            }
+            return ConnectionCache[connectionString];
+        }
+
+        /// <summary>
+        /// 构建链接,返回对象
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        private static ConnectionMultiplexer GetManager(string connectionString = null)
+        {
+            connectionString = connectionString ?? ConfigurationManager.AppSettings["Redis_Host"];
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException("请配置Redis连接串！");
+            }
+            var connect = ConnectionMultiplexer.Connect(connectionString);
+            //注册如下事件
+            connect.ConnectionFailed += MuxerConnectionFailed;
+            connect.ConnectionRestored += MuxerConnectionRestored;
+            connect.ErrorMessage += MuxerErrorMessage;
+            connect.ConfigurationChanged += MuxerConfigurationChanged;
+            connect.HashSlotMoved += MuxerHashSlotMoved;
+            connect.InternalError += MuxerInternalError;
+
+            return connect;
+        }
+
+
+        #region 事件
+
+        /// <summary>
+        /// 配置更改时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConfigurationChanged(object sender, EndPointEventArgs e)
+        {
+            Console.WriteLine("Configuration changed: " + e.EndPoint);
+        }
+
+        /// <summary>
+        /// 发生错误时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerErrorMessage(object sender, RedisErrorEventArgs e)
+        {
+            Console.WriteLine("ErrorMessage: " + e.Message);
+        }
+
+        /// <summary>
+        /// 重新建立连接之前的错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionRestored(object sender, ConnectionFailedEventArgs e)
+        {
+            Console.WriteLine("ConnectionRestored: " + e.EndPoint);
+        }
+
+        /// <summary>
+        /// 连接失败 ， 如果重新连接成功你将不会收到这个通知
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionFailed(object sender, ConnectionFailedEventArgs e)
+        {
+            Console.WriteLine("重新连接：Endpoint failed: " + e.EndPoint + ", " + e.FailureType + (e.Exception == null ? "" : (", " + e.Exception.Message)));
+        }
+
+        /// <summary>
+        /// 更改集群
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerHashSlotMoved(object sender, HashSlotMovedEventArgs e)
+        {
+            Console.WriteLine("HashSlotMoved:NewEndPoint" + e.NewEndPoint + ", OldEndPoint" + e.OldEndPoint);
+        }
+
+        /// <summary>
+        /// redis类库错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerInternalError(object sender, InternalErrorEventArgs e)
+        {
+            Console.WriteLine("InternalError:Message" + e.Exception.Message);
+        }
+
+        #endregion 事件
+
+
+    }
+}
